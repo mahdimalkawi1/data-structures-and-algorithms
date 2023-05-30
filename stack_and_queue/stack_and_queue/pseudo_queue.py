9926e11ab294341c02722e7bf91852084c01485f
mahdimalkawi1 <mahdimalkawi66@gmail.com>
2023-05-30 20:35:07 +0000

    added doc string

--- stack_and_queue/stack_and_queue/pseudo_queue.py
+++ stack_and_queue/stack_and_queue/pseudo_queue.py
@@ -4,14 +4,32 @@ except ImportError:
     from stack_and_queue.stack import Stack
 
 class PseudoQueue:
+    """
+    A class representing a pseudo queue that uses two stacks internally to implement enqueue and dequeue operations.
+    """
     def __init__(self):
+        """
+        Initializes a new instance of the PseudoQueue class.
+        """
         self.stack_1 = Stack()  
         self.stack_2 = Stack()  
 
     def enqueue(self, value):
+        """
+        Adds the specified value to the pseudo queue.
+
+        Args:
+            value: The value to be enqueued.
+        """
         self.stack_1.push(value)
 
     def dequeue(self):
+        """
+        Removes and returns the first element from the pseudo queue.
+
+        Raises:
+            Exception: If the queue is empty.
+        """
         if self.stack_1.is_empty():
             raise Exception("The queue is empty")
 
@@ -25,6 +43,12 @@ class PseudoQueue:
                 self.stack_1.push(self.stack_2.pop())
 
     def __str__(self):
+        """
+        Returns a string representation of the pseudo queue.
+
+        Returns:
+            A string representation of the pseudo queue.
+        """
         elements = []
         while not self.stack_1.is_empty():
             elements.append(self.stack_1.pop())
